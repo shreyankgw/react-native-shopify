d357e638548788038bcff27dcc9003a0eea77556
Shreyank Jadiya <sjadiya@greenworkstools.com>
2025-02-04 13:26:49 -0500

    predictive search

--- app/search/index.tsx
+++ app/search/index.tsx
@@ -1,38 +1,97 @@
-import React from "react";
-import { View, Text, ScrollView, TouchableOpacity, SafeAreaView, TextInput } from "react-native";
+import React, {useState, useCallback, useRef } from "react";
+import { View, Text, ScrollView, TouchableOpacity, SafeAreaView, TextInput, Animated, ActivityIndicator } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { router } from "expo-router";
+import { predictiveSearch } from "@/lib/shopifyQueries";
+import debounce from "lodash.debounce";
+import SearchResultSection from "@/components/SearchResultSection";
+
+interface SearchResult{
+    products?: any[];
+    collections?: any[];
+    pages?: any[];
+}
 
 export default function Search() {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [results, setResults] = useState<SearchResult | null>(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+    const fadeAnim = useRef(new Animated.Value(0)).current;
+
+    const fetchSearchResults = useCallback(async (query: string) => {
+        if(!query){
+            setResults(null);
+            return;
+        }
+       setLoading(true);
+       setError(null);
+
+       try{
+        const data = await predictiveSearch(query, "US", "EN");
+
+        setResults(data);
+        Animated.timing(fadeAnim, {
+            toValue: 1,
+            duration: 300,
+            useNativeDriver: true
+        }).start()
+       }catch(error){
+        setError("Error fetching search results");
+        console.error(error);
+       }finally{
+        setLoading(false);
+       }
+
+    }, []);
+
+    const debouncedSearch = useCallback(debounce((query: string) => fetchSearchResults(query), 300), []);
+
+    const handleSearch = (query: string) => {
+        setSearchQuery(query);
+        debouncedSearch(query);
+    }
+
 
     return (
         <SafeAreaView className="bg-white h-full">
-            <View>
-                <ScrollView className="bg-white h-full">
+            <View className="flex-1">
                     <View className="flex flex-row items-center justify-between p-4 border-b-2 border-gray-200 w-full gap-4">
                         <TouchableOpacity className="text-3xl font-mBold" onPress={() => router.back()}><Ionicons name="arrow-back-outline" size={24} color="black" /></TouchableOpacity>
                         <View className="flex-1">
                             <View className="flex-row items-center justify-between bg-gray-50 rounded-lg border border-brandText px-4 py-2">
                                 <TextInput
-                                    className="text-base text-brandText"
+                                    className="text-base text-brandText flex-1"
                                     placeholder="Search products..."
                                     placeholderTextColor="#24272a"
                                     accessibilityLabel="Search"
-                                    onFocus={() => { }}
+                                    value={searchQuery}
+                                    onChangeText={handleSearch}
+                                    autoFocus
                                 />
-                                <TouchableOpacity onPress={() => { }} className="pl-2">
-                                    <Ionicons name="search-outline" size={20} color="#24272a" className="mr-2" />
-                                </TouchableOpacity>
+                                {loading ? ( <ActivityIndicator size="small" color="#24272a" /> ) : ( <Ionicons name="search-outline" size={20} color="#24272a" className="mr-2" /> ) }
                             </View>
                         </View>
-                        <TouchableOpacity className="text-3xl font-mBold" onPress={() => router.push("/(tabs)/cart")}><Ionicons name="bag-outline" size={24} color="black" /></TouchableOpacity>
+                        <TouchableOpacity className="text-3xl font-mBold" onPress={() => router.push("/(tabs)/cart")}>
+                            <Ionicons name="bag-outline" size={24} color="black" />
+                        </TouchableOpacity>
                     </View>
-                    <View>
-                        <View className="w-full">
-                               
-                        </View>
-                    </View>
-                </ScrollView>
+                    <Animated.ScrollView className="bg-white" style={{ opacity: fadeAnim }}>
+                        {error && <Text className="text-red-500 text-center">{error}</Text>}
+
+                        {!loading && !error && results && (
+                            <>
+                              <SearchResultSection title="Products" items={results.products} type="product" />
+                              <SearchResultSection title="Collections" items={results.collections} type="collection" />
+                              <SearchResultSection title="Pages" items={results.pages} type="page" />
+                            </>
+                        )}
+
+                        {!loading && !error && searchQuery && !results && (
+                            <Text className="px-4 py-2 text-gray-500">No results for "{searchQuery}"</Text>
+                        )}
+                    </Animated.ScrollView>
+                
             </View>
         </SafeAreaView>
     );
---
+++ components/SearchResultSection.tsx
@@ -0,0 +1,20 @@
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+
+export default function SearchResultSection({title, items, type}: {title: string, items: any[] | undefined, type: string}){
+ if(items && !items.length) return null;
+
+ return(
+       <View className="mb-6">
+            <Text className="text-lg font-mBold px-4 mb-2">{title}</Text>
+            {items && items.map((item) => (
+                <TouchableOpacity key={item.id} className="px-4 py-3 flex-row items-center" onPress={() => router.push(type === "product" ? `/products/${item.handle}` : `/collections/${item.handle}`)}>
+                    {item.featuredImage && item.featuredImage.url && <Image source={{ uri: item.featuredImage.url }} className="w-12 h-12 rounded-md mr-3" resizeMode="contain" />}
+                    <Text className="text-base flex-1">{item.title}</Text>
+                    <Ionicons name="chevron-forward" size={24} color="#666" />
+                </TouchableOpacity>
+            ))}
+       </View>
+ )
+}
