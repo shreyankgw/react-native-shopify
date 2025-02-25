2a5286acbc81eb090b5608cf02116c55cc46c617
Shreyank Jadiya <sjadiya@greenworkstools.com>
2025-02-25 14:54:37 -0500

    profile page changes

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,14 +1,38 @@
-import { Text, View, ScrollView } from "react-native";
+import { Text, View, ScrollView, TouchableOpacity } from "react-native";
 import React, { useState, useEffect, useRef } from "react";
 import { buildAuthorizationUrl, requestAccessToken, generateCodeVerifier } from "@/lib/shopifyClient";
 import { WebView }  from 'react-native-webview';
 import { fetchCustomerProfile } from "@/lib/shopifyQueries";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { router } from "expo-router";
+
+interface Customer{
+  firstName: string;
+  lastName: string;
+  emailAddress: {
+    emailAddress: string;
+  };
+  defaultAddress: {
+    address1: string;
+    address2: string;
+    city: string;
+    company: string;
+    country: string;
+    name: string;
+    phoneNumber: string;
+    province: string;
+    zoneCode: string;
+    zip: string;
+  };
+}
 
 export default function Profile() {
   const [accessToken, setAccessToken] = useState(null);
   const [idToken, setIdToken] = useState(null);
   const [refreshToken, setRefreshToken] = useState(null);
-  const [customer, setCustomer] = useState(null);
+  const [customer, setCustomer] = useState<Customer | null>(null);
   const [authUrl, setAuthUrl] = useState<string | null>(null);
   const [codeVerifier, setCodeVerifier] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
@@ -92,6 +116,7 @@ export default function Profile() {
 
   if(!accessToken && authUrl && !isLoading){
     return (
+      <SafeAreaView className="flex-1 bg-white px-4">
       <WebView 
         ref={webViewRef}
         source={{ uri: authUrl }}
@@ -108,41 +133,76 @@ export default function Profile() {
           console.log('WebView HTTP Error:', syntheticEvent.nativeEvent);
         }}
       />
+      </SafeAreaView>
     )
   } else if(accessToken){
-    // fetch customer details from the API
+    // display customer details and allow them to edit their details and view their order history
 
     return(
-      <View
-      className="flex-1 bg-white px-4"
-    >
-      <Text>Welcome, {customer?.firstName}! Your access token is {accessToken}.</Text>
-      <Text>Your Email is : {customer?.emailAddress?.emailAddress}</Text>
-      {customer?.orders?.edges?.map((order) => (
-        <View key={order.node.id}>
-          <Text>Order ID: {order.node.name}</Text>
-        </View>
-      ))}
+      <SafeAreaView className="bg-white h-full">
+      <ScrollView
+       className="h-full bg-white"
+      >
+      <View className="flex flex-row items-center justify-between p-4 border-b-2 border-gray-200 w-full">
+               <TouchableOpacity className="text-3xl font-mBold" onPress={() => router.back()}><Ionicons name="arrow-back-outline" size={24} color="black" /></TouchableOpacity>
+               <Text className="text-xl font-mBold uppercase">{customer ? "Account" : "Login/Signup" }</Text>
+               <TouchableOpacity className="text-3xl font-mBold" onPress={() => console.log("help faq route for general enquiries")}><Ionicons name="information-circle-outline" size={24} color="black" /></TouchableOpacity>
+      </View>
+      <View className="flex flex-row items-center justify-between p-4">
+       <Text className="text-2xl font-mBold">Hi, {customer && customer.firstName ? customer.firstName : customer?.emailAddress?.emailAddress} !</Text>
+      </View>
+
+      <View className="px-4">
+         <TouchableOpacity className="flex flex-row items-center justify-between p-4 border-b-2 border-gray-200 w-full" onPress={() => router.push("/account/edit")}>
+          <Text className="text-lg font-mSemiBold">Edit Profile</Text>
+          <Ionicons name="chevron-forward-outline" size={24} color="black" />
+         </TouchableOpacity>
+         <TouchableOpacity className="flex flex-row items-center justify-between p-4 border-b-2 border-gray-200 w-full" onPress={() => router.push("/account/orders")}>
+          <Text className="text-lg font-mSemiBold">Order History</Text>
+          <Ionicons name="chevron-forward-outline" size={24} color="black" />
+         </TouchableOpacity>
+      </View>
+
+      <View className="px-4 mt-12">
+         {/* recently viewed products */}
+         <Text className="text-xl font-mBold mt-4">Recently Viewed</Text>
+         {/* render product grid of recently viewed products that are stored in mmkv local storage */ }
+      </View>
+
+      <View className="px-4 mt-12 flex flex-row items-center justify-between">
+         <TouchableOpacity className="flex flex-row items-center justify-center gap-1 bg-darkPrimary px-4 py-2 rounded-lg" onPress={() => console.log("logout customer mutation")}>
+          <Ionicons name="log-out-outline" size={24} color="white" />
+          <Text className="text-lg font-mSemiBold text-white">Logout</Text>
+         </TouchableOpacity>
+
+         <TouchableOpacity className="flex flex-row items-center justify-center gap-1 bg-red-500 px-4 py-2 rounded-lg" onPress={() => router.push("/support")}>
+         <MaterialIcons name="delete" size={24} color="white" />
+          <Text className="text-lg font-mSemiBold text-white">Delete Account</Text>
+         </TouchableOpacity>
+      </View>
       {/* Add more profile details here */}
-    </View>
+      </ScrollView>
+      </SafeAreaView>  
     )
   }else if (isLoading) {
     return (
-      <View className="flex-1 bg-white px-4 justify-center items-center">
+      <SafeAreaView>
+       <View className="flex-1 bg-white px-4 justify-center items-center">
         <Text>Processing authentication...</Text>
       </View>
+      </SafeAreaView>
+    
     );
   }
   else{
     return (
+      <SafeAreaView>
       <View
         className="flex-1 bg-white px-4"
       >
-        {authUrl && <Text>{authUrl}</Text>}
-         <ScrollView>
-           <Text>No access token found. Redirecting to login ....</Text>
-         </ScrollView>
+           <Text>No profile found. Redirecting to login ....</Text> 
       </View>
+      </SafeAreaView>
     );
   }
 
